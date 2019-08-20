e296b3ab2a06f6add034268bb459ade6bbefd9e1
dmitriyl99 <dmitriyl1899@gmail.com>
2019-08-21 00:16:24 +0500

    Add routes for HandbookCategoriesController

--- routes/admin.php
+++ routes/admin.php
@@ -13,7 +13,9 @@ Route::middleware('checkIsAdmin')->prefix('admin')->name('admin.')->namespace('A
 
     // Resources
     Route::resource('/cgucategories', 'CguCategoryController');
+    Route::resource('/handbookcategories', 'HandbookCategoryController');
 
     //Simple Routes
     Route::get('/cgucategories/{id}/image', 'CguCategoryController@removeImage')->name('cgucategories.remove.image');
+    Route::get('/handbookcategories/{id}/removeImage', 'HandbookCategoryController@removeImage')->name('handbookcategories.remove.image');
 });
