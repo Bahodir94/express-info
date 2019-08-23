da92947b676c8bc54179919a55f0766c49480fdf
Дмитрий <dmitriyl1899@gmail.com>
2019-08-23 18:09:57 +0500

    Fix forms

--- resources/views/admin/pages/needs/create.blade.php
+++ resources/views/admin/pages/needs/create.blade.php
@@ -12,9 +12,8 @@
         ],
         'lastTitle' => 'Добавить потребность'
     ])
-    <form action="" method="post">
+    <form action="{{ route('admin.needs.store') }}" method="post">
         @csrf
-        @method('put')
         <div class="block">
             <div class="block-header block-header-default">
                 <h3 class="block-title">Добавить потребность</h3>
@@ -22,70 +21,70 @@
                     <button class="btn btn-sm btn-alt-success" type="submit"><i class="fa fa-check"></i> Сохранить</button>
                     <button class="btn btn-sm btn-alt-success" type="submit" name="saveQuit"><i class="fa fa-check"></i> Сохранить и выйти</button>
                 </div>
-                <div class="block-content">
-                    <div class="wizard-block">
-                        <ul class="nav nav-tabs nav-tabs-block nav-fill" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#wizard-simple-step1" data-toggle="tab">1. Русский</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#wizard-simple-step2" data-toggle="tab">2. Английский</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#wizard-simple-step3" data-toggle="tab">3. Узбекский</a>
-                            </li>
-                        </ul>
-                        <!-- END Step Tabs -->
+            </div>
+            <div class="block-content">
+                <div class="wizard-block">
+                    <ul class="nav nav-tabs nav-tabs-block nav-fill" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#wizard-simple-step1" data-toggle="tab">1. Русский</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#wizard-simple-step2" data-toggle="tab">2. Английский</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#wizard-simple-step3" data-toggle="tab">3. Узбекский</a>
+                        </li>
+                    </ul>
+                    <!-- END Step Tabs -->
 
-                        <!-- Steps Content -->
-                        <div class="block-content block-content-full tab-content">
-                            <!-- Step 1 -->
-                            <div class="tab-pane active" id="wizard-simple-step1" role="tabpanel">
-                                <div class="form-group @error('ru_title') is-invalid @enderror">
-                                    <div class="form-material floating">
-                                        <label for="ru_title" @error('ru_title') class="col-form-label" @enderror>
-                                            Заголовок
-                                            @error('ru_title') <span class="text-danger">*</span> @enderror
-                                        </label>
-                                        <input class="form-control" type="text" id="ru_title" name="ru_title" value="{{ old('ru_title') }}">
-                                    </div>
-                                    @error('ru_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
+                    <!-- Steps Content -->
+                    <div class="block-content block-content-full tab-content">
+                        <!-- Step 1 -->
+                        <div class="tab-pane active" id="wizard-simple-step1" role="tabpanel">
+                            <div class="form-group @error('ru_title') is-invalid @enderror">
+                                <div class="form-material floating">
+                                    <label for="ru_title" @error('ru_title') class="col-form-label" @enderror>
+                                        Заголовок
+                                        @error('ru_title') <span class="text-danger">*</span> @enderror
+                                    </label>
+                                    <input class="form-control" type="text" id="ru_title" name="ru_title" value="{{ old('ru_title') }}">
                                 </div>
+                                @error('ru_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
                             </div>
-                            <!-- END Step 1 -->
-
-                            <!-- Step 2 -->
-                            <div class="tab-pane" id="wizard-simple-step2" role="tabpanel">
-                                <div class="form-group @error('en_title') is-invalid @enderror">
-                                    <div class="form-material floating">
-                                        <label for="uz_title" @error('en_title') class="col-form-label" @enderror>
-                                            Заголовок
-                                            @error('en_title') <span class="text-danger">*</span> @enderror
-                                        </label>
-                                        <input class="form-control" type="text" id="en_title" name="en_title" value="{{ old('en_title') }}">
-                                    </div>
-                                    @error('en_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
-                                </div>
-                            </div>
-                            <!-- END Step 2 -->
-
-                            <!-- Step 3 -->
-                            <div class="tab-pane" id="wizard-simple-step3" role="tabpanel">
-                                <div class="form-group @error('uz_title') is-invalid @enderror">
-                                    <div class="form-material floating">
-                                        <label for="uz_title" @error('uz_title') class="col-form-label" @enderror>
-                                            Заголовок
-                                            @error('uz_title') <span class="text-danger">*</span> @enderror
-                                        </label>
-                                        <input class="form-control" type="text" id="uz_title" name="uz_title" value="{{ old('uz_title') }}">
-                                    </div>
-                                    @error('uz_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
-                                </div>
-                            </div>
-                            <!-- END Step 3 -->
                         </div>
-                        <!-- END Steps Content -->
+                        <!-- END Step 1 -->
+
+                        <!-- Step 2 -->
+                        <div class="tab-pane" id="wizard-simple-step2" role="tabpanel">
+                            <div class="form-group @error('en_title') is-invalid @enderror">
+                                <div class="form-material floating">
+                                    <label for="uz_title" @error('en_title') class="col-form-label" @enderror>
+                                        Заголовок
+                                        @error('en_title') <span class="text-danger">*</span> @enderror
+                                    </label>
+                                    <input class="form-control" type="text" id="en_title" name="en_title" value="{{ old('en_title') }}">
+                                </div>
+                                @error('en_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
+                            </div>
+                        </div>
+                        <!-- END Step 2 -->
+
+                        <!-- Step 3 -->
+                        <div class="tab-pane" id="wizard-simple-step3" role="tabpanel">
+                            <div class="form-group @error('uz_title') is-invalid @enderror">
+                                <div class="form-material floating">
+                                    <label for="uz_title" @error('uz_title') class="col-form-label" @enderror>
+                                        Заголовок
+                                        @error('uz_title') <span class="text-danger">*</span> @enderror
+                                    </label>
+                                    <input class="form-control" type="text" id="uz_title" name="uz_title" value="{{ old('uz_title') }}">
+                                </div>
+                                @error('uz_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
+                            </div>
+                        </div>
+                        <!-- END Step 3 -->
                     </div>
+                    <!-- END Steps Content -->
                 </div>
             </div>
         </div>
--- resources/views/admin/pages/needs/index.blade.php
+++ resources/views/admin/pages/needs/index.blade.php
@@ -17,13 +17,16 @@
             <ul class="list-group list-group-flush mb-20">
                 @foreach($needs as $need)
                     <li class="list-group-item d-flex justify-content-between align-items-center">{{ $need->ru_title }}
-                        <a href="{{ route('admin.needs.edit', $need->id) }}" data-toggle="tooltip" title="Редактировать"
-                           class="btn btn-sm btn-alt-info"><i class="fa fa-edit"></i></a>
-                        <form action="" method="post">
-                            @csrf
-                            @method('delete')
-                            <button data-toggle="tooltip" onclick="return confirm('Вы уверены?')" title="Удалить" class="btn btn-sm btn-alt-danger"><i class="fa fa-trash"></i></button>
-                        </form></li>
+                        <span class="d-flex">
+                            <a href="{{ route('admin.needs.edit', $need->id) }}" data-toggle="tooltip" title="Редактировать"
+                               class="btn btn-sm btn-alt-info mr-10"><i class="fa fa-edit"></i></a>
+                            <form action="" method="post">
+                                @csrf
+                                @method('delete')
+                                <button data-toggle="tooltip" onclick="return confirm('Вы уверены?')" title="Удалить" class="btn btn-sm btn-alt-danger"><i class="fa fa-trash"></i></button>
+                            </form>
+                        </span>
+                    </li>
                 @endforeach
             </ul>
         </div>
