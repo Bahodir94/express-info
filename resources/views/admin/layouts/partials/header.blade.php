689e381b97a9e880dfcdabf481d0a4189414bb97
dmitriyl99 <dmitriyl1899@gmail.com>
2019-08-27 15:35:01 +0500

    Links for logout

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -10,62 +10,7 @@
                 <i class="fa fa-navicon"></i>
             </button>
             <!-- END Toggle Sidebar -->
-
-            <!-- Open Search Section -->
-            <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-            <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="header_search_on">
-                <i class="fa fa-search"></i>
-            </button>
-            <!-- END Open Search Section -->
-
-            <!-- Color Themes (used just for demonstration) -->
-            <!-- Themes functionality initialized in Codebase() -> uiHandleTheme() -->
-            <div class="btn-group" role="group">
-                <button type="button" class="btn btn-circle btn-dual-secondary" id="page-header-themes-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fa fa-paint-brush"></i>
-                </button>
-                <div class="dropdown-menu min-width-150" aria-labelledby="page-header-themes-dropdown">
-                    <h6 class="dropdown-header text-center">Color Themes</h6>
-                    <div class="row no-gutters text-center mb-5">
-                        <div class="col-4 mb-5">
-                            <a class="text-default" data-toggle="theme" data-theme="default" href="javascript:void(0)">
-                                <i class="fa fa-2x fa-circle"></i>
-                            </a>
-                        </div>
-                        <div class="col-4 mb-5">
-                            <a class="text-elegance" data-toggle="theme" data-theme="assets/css/themes/elegance.min.css" href="javascript:void(0)">
-                                <i class="fa fa-2x fa-circle"></i>
-                            </a>
-                        </div>
-                        <div class="col-4 mb-5">
-                            <a class="text-pulse" data-toggle="theme" data-theme="assets/css/themes/pulse.min.css" href="javascript:void(0)">
-                                <i class="fa fa-2x fa-circle"></i>
-                            </a>
-                        </div>
-                        <div class="col-4 mb-5">
-                            <a class="text-flat" data-toggle="theme" data-theme="assets/css/themes/flat.min.css" href="javascript:void(0)">
-                                <i class="fa fa-2x fa-circle"></i>
-                            </a>
-                        </div>
-                        <div class="col-4 mb-5">
-                            <a class="text-corporate" data-toggle="theme" data-theme="assets/css/themes/corporate.min.css" href="javascript:void(0)">
-                                <i class="fa fa-2x fa-circle"></i>
-                            </a>
-                        </div>
-                        <div class="col-4 mb-5">
-                            <a class="text-earth" data-toggle="theme" data-theme="assets/css/themes/earth.min.css" href="javascript:void(0)">
-                                <i class="fa fa-2x fa-circle"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="dropdown-divider"></div>
-                    <button type="button" class="btn btn-sm btn-block btn-alt-secondary mb-10" data-toggle="layout" data-action="sidebar_style_inverse_toggle">Sidebar Style</button>
-                </div>
-            </div>
-            <!-- END Color Themes -->
         </div>
-        <!-- END Left Section -->
-
         <!-- Right Section -->
         <div class="content-header-section">
             <!-- User Dropdown -->
@@ -74,29 +19,12 @@
                     {{ \Auth::user()->name }}<i class="fa fa-angle-down ml-5"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-right min-width-150" aria-labelledby="page-header-user-dropdown">
-                    <a class="dropdown-item" href="#">
-                        <i class="si si-user mr-5"></i> Profile
-                    </a>
-                    <a class="dropdown-item d-flex align-items-center justify-content-between" href="#">
-                        <span><i class="si si-envelope-open mr-5"></i> Inbox</span>
-                        <span class="badge badge-primary">3</span>
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="si si-note mr-5"></i> Invoices
-                    </a>
-                    <div class="dropdown-divider"></div>
-
-                    <!-- Toggle Side Overlay -->
-                    <!-- Layout API, functionality initialized in Codebase() -> uiApiLayout() -->
-                    <a class="dropdown-item" href="javascript:void(0)" data-toggle="layout" data-action="side_overlay_toggle">
-                        <i class="si si-wrench mr-5"></i> Settings
-                    </a>
-                    <!-- END Side Overlay -->
-
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">
-                        <i class="si si-logout mr-5"></i> Sign Out
-                    </a>
+                    <form action="{{ route('admin.logout') }}" method="post">
+                        @csrf
+                        <button class="dropdown-item">
+                            <i class="si si-logout mr-5"></i> Выйти
+                        </button>
+                    </form>
                 </div>
             </div>
             <!-- END User Dropdown -->
