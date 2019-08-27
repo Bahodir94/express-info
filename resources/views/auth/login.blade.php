ff3511d41b7e3892d406201816ebfb62d78e3231
dmitriyl99 <dmitriyl1899@gmail.com>
2019-08-27 15:11:58 +0500

    Login page for admin panel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,111 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <title>Вход | ExpressInfo</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <meta name="description" content="Административная панель ExpressInfo">
+    <meta name="author" content="VID">
+    <meta name="robots" content="noindex, nofollow">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicons/favicon.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/img/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicons/apple-touch-icon-180x180.png') }}">
+    <!-- END Icons -->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <!-- Stylesheets -->
+    <!-- Codebase framework -->
+    <link rel="stylesheet" id="css-main" href="{{ asset('assets/css/codebase.min.css') }}">
+</head>
+<body>
+<!-- Page Container -->
+<div id="page-container" class="main-content-boxed">
+    <!-- Main Container -->
+    <main id="main-container">
+        <!-- Page Content -->
+        <div class="bg-image" style="background-image: url('{{ asset('assets/img/photos/photo34@2x.jpg') }}');">
+            <div class="row mx-0 bg-black-op">
+                <div class="hero-static col-md-6 col-xl-8 d-none d-md-flex align-items-md-end">
+                    <div class="p-30 invisible" data-toggle="appear">
+                        <p class="font-size-h3 font-w600 text-white">
+                            Get Inspired and Create.
+                        </p>
+                        <p class="font-italic text-white-op">
+                            Copyright &copy; <span class="js-year-copy">2017</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="hero-static col-md-6 col-xl-4 d-flex align-items-center bg-white invisible" data-toggle="appear" data-class="animated fadeInRight">
+                    <div class="content content-full">
+                        <!-- Header -->
+                        <div class="px-30 py-10">
+                            <a class="link-effect font-w700" href="#">
+                                <i class="si si-fire"></i>
+                                <span class="font-size-xl text-primary-dark">Express</span><span class="font-size-xl">Info</span>
+                            </a>
+                            <h1 class="h3 font-w700 mt-30 mb-10">Добро пожаловать в административную панель ExpressInfo</h1>
+                            <h2 class="h5 font-w400 text-muted mb-0">Пожалуйста, войдите</h2>
                         </div>
+                        <!-- END Header -->
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        <!-- Sign In Form -->
+                        <!-- jQuery Validation (.js-validation-signin class is initialized in js/pages/op_auth_signin.js) -->
+                        <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                        <form class="px-30" action="{{ route('admin.login') }}" method="post">
+                            @csrf
+                            <div class="form-group row @error('email') is-invalid @enderror">
+                                <div class="col-12">
+                                    <div class="form-material floating">
+                                        <input type="email" class="form-control" id="login-username" name="email" value="{{ old('email') }}">
+                                        <label for="login-username">Email</label>
+                                        @error('email') <div class="invalid-feedback">{{ $message }}</div>@enderror
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                            <div class="form-group row @error('password') is-invalid @enderror">
+                                <div class="col-12">
+                                    <div class="form-material floating">
+                                        <input type="password" class="form-control" id="login-password" name="password">
+                                        <label for="login-password">Пароль</label>
+                                        @error('password') <div class="invalid-feedback">{{ $message }}</div>@enderror
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <label class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="login-remember-me" name="remember">
+                                        <span class="custom-control-indicator"></span>
+                                        <span class="custom-control-description">Запомнить меня</span>
                                     </label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-sm btn-hero btn-alt-primary">
+                                    <i class="si si-login mr-10"></i> Войти
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                        <!-- END Sign In Form -->
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        <!-- END Page Content -->
+    </main>
+    <!-- END Main Container -->
 </div>
-@endsection
+<!-- END Page Container -->
+
+<!-- Codebase Core JS -->
+<script src="{{ asset('assets/js/core/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/core/jquery.appear.min.js') }}"></script>
+<script src="{{ asset('assets/js/codebase.js') }}"></script>
+</body>
+</html>
