4e3807caab1f45d4294ebfde8d016f4eeb9e7363
dmitriyl99 <dmitriyl1899@gmail.com>
2019-08-31 22:48:57 +0500

    Components for client side

---
+++ resources/views/site/components/search.blade.php
@@ -0,0 +1,10 @@
+<!-- Search section -->
+<div class="uk-container uk-container-large uk-container-center uk-margin-top">
+    <form action="{{ route('site.catalog.search') }}" class="uk-center">
+        <div class="position">
+            <i class="fa fa-search"></i>
+            <input type="text" placeholder="Поиск в Айко">
+        </div>
+    </form>
+</div>
+<!-- Search section end -->
