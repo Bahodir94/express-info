344a9470be45ff36e772fc0045cc662e75ae4c20
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-02 02:09:06 +0500

    Catalog controller

---
+++ app/Http/Controllers/Site/CatalogController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Repositories\CompanyRepositoryInterface;
+use App\Repositories\HandbookCategoryRepositoryInterface;
+use App\Repositories\NeedTypeRepositoryInterface;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CatalogController extends Controller
+{
+    /**
+     * Needs repository
+     *
+     * @var NeedTypeRepositoryInterface
+    */
+    private $needs;
+
+    /**
+     * Category repository
+     *
+     * @var HandbookCategoryRepositoryInterface
+    */
+    private $categories;
+
+    /**
+     * Company repository
+     *
+     * @var CompanyRepositoryInterface
+    */
+    private $companies;
+
+    /**
+     * Create a new controller instance
+     *
+     * @param NeedTypeRepositoryInterface $needsRepository
+     * @param HandbookCategoryRepositoryInterface $categoriesRepository
+     * @param CompanyRepositoryInterface $companyRepository
+     * @return void
+    */
+    public function __construct(NeedTypeRepositoryInterface $needsRepository,
+                                HandbookCategoryRepositoryInterface $categoriesRepository,
+                                CompanyRepositoryInterface $companyRepository)
+    {
+        $this->needs = $needsRepository;
+        $this->categories = $categoriesRepository;
+        $this->companies = $companyRepository;
+    }
+
+    /**
+     * Main page of catalog
+     *
+     * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        $needs = $this->needs->all();
+        $favoritesCategories = $this->categories->getFavoriteCategories();
+        $parentCategories = $this->categories->all();
+
+        return view('site.pages.catalog.index', compact('needs',
+            'favoritesCategories', 'parentCategories'));
+    }
+
+    /**
+     * Show concrete category
+     *
+     * @param Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+    */
+    public function category(Request $request, int $id)
+    {
+        $category = $this->categories->get($id);
+        $descendantsCategories = $category->descendants;
+        $companies = [];
+        $resultCompanies = [];
+        array_push($companies, $category->companies);
+        foreach ($descendantsCategories as $descendantsCategory)
+            array_push($companies, $descendantsCategory->companies);
+        if ($request->has('service'))
+        {
+            $serviceId = $request->get('service');
+            foreach ($companies as $company)
+                if ($company->hasService($serviceId))
+                    array_push($resultCompanies, $company);
+        }
+        else
+            $resultCompanies = $companies;
+
+        $data = [
+            'category' => $category,
+            'companies' => $resultCompanies
+        ];
+
+        return view('site.pages.catalog.companies', $data);
+    }
+
+    /**
+     * Show company page
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+    */
+    public function company(int $id)
+    {
+        $company = $this->companies->get($id);
+
+        return view('site.pages.catalog.company', compact('company'));
+    }
+}
