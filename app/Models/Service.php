b17982464b90cc0c74db346c3209bb3bd1fee865
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-01 23:57:37 +0500

    Method for images

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Service extends Model
 {
@@ -26,5 +27,74 @@ class Service extends Model
     public function category()
     {
         return $this->hasOne(HandbookCategory::class, 'id', 'category_id');
+    }
+
+    /**
+     * Upload an image and save it in file storage
+     *
+     * @param $image
+     */
+    public function uploadImage($image)
+    {
+        if (!$image) return;
+
+        $this->removeImage();
+        $filename = $this->generateFileName($image->extension());
+        $image->storeAs(self::UPLOAD_DIRECTORY, $filename);
+        $this->saveImageName($filename);
+    }
+
+    /**
+     * Generate filename for image
+     *
+     * @param string $imageName
+     * @return string
+     */
+    private function generateFileName(string $imageName)
+    {
+        return str_random(20) . $imageName;
+    }
+
+    /**
+     * Get image filename
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        if ($this->image)
+            return '/' . self::UPLOAD_DIRECTORY . $this->image;
+        else
+            return '';
+    }
+
+    /**
+     * Remove an image
+     *
+     * @return void
+     */
+    public function removeImage()
+    {
+        if ($this->image) {
+            Storage::delete(self::UPLOAD_DIRECTORY . $this->image);
+        }
+    }
+
+    /**
+     * Save an image name to the database
+     *
+     * @param string $imageName
+     * @return void
+     */
+    private function saveImageName(string $imageName)
+    {
+        $this->image = $imageName;
+        $this->save();
+    }
+
+    public function delete()
+    {
+        $this->removeImage();
+        parent::delete();
     }
 }
