e6a2afb10ee69c6c085138ae68cf2ffee6ae2a6c
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-05 02:17:15 +0500

    Update client side assets

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,32 +1,4 @@
 window.onload=function(){
-
-    // autoSlider ();
-    // var left = 0;
-    // var timer;
-    // function autoSlider () {
-    //     timer = setTimeout (function (){
-    //         var slider_wrapper = document.querySelector('.slider .slider_wrapper');
-    //         left = left - 240;
-    //         if (left < -1200){
-    //             left = 0;
-    //             clearTimeout(timer);
-    //         }
-    //         slider_wrapper.style.left = left  + 'px';
-    //         autoSlider();
-    //     }, 5000);
-    // }
-
-
-    $('.slider_wrapper').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-      });
-
-
-
-
     var menuElem = document.getElementById('dropdown-menu'),
         titleElem = menuElem.querySelector('.dropdown_title');
         document.onclick = function(event) {
