f4c301d65d01721585aecdcc678f560b117b4aee
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-04 18:20:31 +0500

    Create relationship for categories and menu items

--- app/Models/HandbookCategory.php
+++ app/Models/HandbookCategory.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\Components\Image;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
 use Kalnoy\Nestedset\NodeTrait;
 
 class HandbookCategory extends Model
 {
     use NodeTrait;
+    use Image;
 
     protected $fillable = [
-        'ru_title', 'en_title', 'uz_title', 'ru_slug', 'en_slug', 'uz_slug', 'parent_id',
-        'need_id'
+        'ru_title', 'en_title', 'uz_title', 'ru_slug', 'en_slug', 'uz_slug', 'parent_id', 'menu_id'
     ];
 
     const UPLOAD_DIRECTORY = 'uploads/handbook_categories_images/';
@@ -96,66 +96,13 @@ class HandbookCategory extends Model
     }
 
     /**
-     * Upload an image and save it in file storage
+     * Menu item for thi category
      *
-     * @param $image
-    */
-    public function uploadImage($image)
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function menu()
     {
-        if (!$image) return;
-
-        $this->removeImage();
-        $filename = $this->generateFileName($image->extension());
-        $image->storeAs(self::UPLOAD_DIRECTORY, $filename);
-        $this->saveImageName($filename);
-    }
-
-    /**
-     * Generate filename for image
-     *
-     * @param string $imageName
-     * @return string
-    */
-    private function generateFileName(string $imageName)
-    {
-        return str_random(20) . $imageName;
-    }
-
-    /**
-     * Get image filename
-     *
-     * @return string
-    */
-    public function getImage()
-    {
-        if ($this->image)
-            return '/' . self::UPLOAD_DIRECTORY . $this->image;
-        else
-            return '';
-    }
-
-    /**
-     * Remove an image
-     *
-     * @return void
-    */
-    public function removeImage()
-    {
-        if ($this->image) {
-            Storage::delete(self::UPLOAD_DIRECTORY . $this->image);
-        }
-    }
-
-    /**
-     * Save an image name to the database
-     *
-     * @param string $imageName
-     * @return void
-    */
-    private function saveImageName(string $imageName)
-    {
-        $this->image = $imageName;
-        $this->save();
+        return $this->hasOne(MenuItem::class, 'id', 'menu_id');
     }
 
     /**
