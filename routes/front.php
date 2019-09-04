871cb065b2d57a7df9a0ca74f32323ad963a9d8a
Дмитрий <dmitriyl1899@gmail.com>
2019-09-04 12:53:55 +0500

    CGU routes

--- routes/front.php
+++ routes/front.php
@@ -12,3 +12,9 @@ Route::middleware('needsList')->name('site.')->namespace('Site')->group(function
     Route::get('/catalog/category/{id}', 'CatalogController@category')->name('catalog.category');
     Route::get('/catalog/company/{id}', 'CatalogController@company')->name('catalog.company');
 });
+
+
+Route::get('/cgu-info', 'Site\CguController@cguInfo')->name('home.cgu.info');
+Route::get('/cgu-info/{id}', 'Site\CguController@cguCategory')->name('home.cgu.info.category');
+Route::get('/cgu-ad', 'Site\CguController@cguAd')->name('home.cgu.ad');
+Route::get('/cgu-ad/{id}', 'Site\CguController@cguCategory')->name('home.cgu.ad.category');
