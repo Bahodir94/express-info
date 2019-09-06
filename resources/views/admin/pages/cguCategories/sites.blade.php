8894ad7cd3c1c1066d17cd4764071ecf891c87e1
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-06 17:55:40 +0500

    Fix positions in CGU

--- resources/views/admin/pages/cguCategories/sites.blade.php
+++ resources/views/admin/pages/cguCategories/sites.blade.php
@@ -81,13 +81,6 @@
     <script src="{{ asset('assets/js/plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
     <script>
-        $('.js-dataTable-full').dataTable({
-            "order": [],
-            pageLength: 10,
-            lengthMenu: [[10, 20, 30, 50], [10, 20, 30, 50]],
-            autoWidth: true,
-            language: ru_datatable
-        });
         $('.position').change(function () {
             $.ajaxSetup({
                 headers: {
@@ -115,6 +108,13 @@
                     console.log(data);
                 }
             });
-        })
+        });
+        $('.js-dataTable-full').dataTable({
+            "order": [],
+            pageLength: 10,
+            lengthMenu: [[10, 20, 30, 50], [10, 20, 30, 50]],
+            autoWidth: true,
+            language: ru_datatable
+        });
     </script>
 @endsection
