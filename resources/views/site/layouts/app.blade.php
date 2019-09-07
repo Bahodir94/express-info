edfabf167a17129cc80c35a6d0c696b68a9464cb
devseylor <48056243+devseylor@users.noreply.github.com>
2019-09-07 17:07:27 +0500

    Изменения в мобильной версии
    Появляеться лого, кого экран меньше 576px

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -28,7 +28,7 @@
 <header>
     <div class="uk-container uk-container-expand uk-container-center">
         <nav class="uk-navbar">
-            <div class="content-header-item uk-visible-large">
+            <div class="content-header-item uk-visible@l">
                 <a class="link-effect font-w700" href="{{ route('home') }}">
                     <span class="icon">
                         <iconify-icon data-icon="simple-line-icons:fire"></iconify-icon>
@@ -36,7 +36,7 @@
                     <span class="font-size-xl text-dual-primary-dark">Tez</span><span class="font-size-xl text-primary">Info</span>
                 </a>
             </div>
-            <div class="uk-navbar-center uk-visible-large">
+            <div class="uk-navbar-center uk-visible@l">
                 <ul class="uk-navbar-nav">
                     <li ><a href="{{ route('site.catalog.index') }}">Главная</a></li>
                     @foreach ($needs as $need)
@@ -63,9 +63,9 @@
                     @endforeach
                 </ul>
             </div>
-            <button type="button" class="menu-button uk-hidden-large" uk-toggle="target: #offcanvas-slide" ><i class="fa fa-bars"></i></button>
+            <button type="button" class="menu-button uk-hidden@l" uk-toggle="target: #offcanvas-slide" ><i class="fa fa-bars"></i></button>
 
-            <div class="content-header-item uk-hidden-large  uk-hidden-small">
+            <div class="content-header-item uk-hidden@l	uk-hidden@s">
                 <a class="link-effect font-w700" href="{{ route('home') }}">
                     <i class="si si-fire text-primary"></i>
                     <span class="font-size-xl text-dual-primary-dark">Tez</span><span class="font-size-xl text-primary">Info</span>
