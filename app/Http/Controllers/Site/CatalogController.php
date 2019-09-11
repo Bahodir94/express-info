d9445b3a8f66f56c65c0b512e400638a3e55342c
Asadkhoja <asadxoja2001@gmail.com>
2019-09-11 18:56:39 +0300

    Fix method signature

--- app/Http/Controllers/Site/CatalogController.php
+++ app/Http/Controllers/Site/CatalogController.php
@@ -118,14 +118,14 @@ class CatalogController extends Controller
     public function search(Request $request)
     {
         $query = $request->get('query');
-        $category = $this->categories->search($query, $findOne = true);
+	$category = $this->categories->search($query, $findOne = true);
         if ($category)
-            return redirect()->route('site.catalog.category', $category->id);
-        $data = [];
-        $categories = $this->categories->search($query);
-        $companies = $this->companies->search($query);
+	    return redirect()->route('site.catalog.category', $category->id);
+	$data = [];
+	$categories = $this->categories->search($query);
+	$companies = $this->companies->search($query);
         $data['categories'] = $categories;
-        $data['companies'] = $companies;
+	$data['companies'] = $companies;
         return view('site.pages.catalog.search', $data);
     }
 }
