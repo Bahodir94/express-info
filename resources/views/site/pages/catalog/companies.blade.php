8a684d1ef216ca9770977e5d1b7967723a3afc77
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-15 22:29:30 +0500

    Slug for parent category

--- resources/views/site/pages/catalog/companies.blade.php
+++ resources/views/site/pages/catalog/companies.blade.php
@@ -129,7 +129,7 @@
 -->
         <ul class="cat-tab uk-tab" >
             <li class="uk-active">
-                <a href="{{ $category->hasParentCategory() ? route('site.catalog.category', $category->parent_id) : route('site.catalog.index') }}">
+                <a href="{{ $category->hasParentCategory() ? route('site.catalog.category', $category->parent->getAncestorsSlugs()) : route('site.catalog.index') }}">
                     <span uk-icon="arrow-left"></span>
                     <span>Назад</span>
                 </a>
