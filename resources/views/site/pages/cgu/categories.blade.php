b1a5247ca8fefd04f68f5cdf74451969221a356f
Дмитрий <dmitriyl1899@gmail.com>
2019-09-17 16:53:39 +0500

    Fix method name

--- resources/views/site/pages/cgu/categories.blade.php
+++ resources/views/site/pages/cgu/categories.blade.php
@@ -74,16 +74,16 @@
                     <div class="main_item" style="background-color: transparent;box-shadow: none;display: flex;padding: 0;">
                         @if($file->video == '')
                             @if(strpos($file->getFileType(), 'image') !== false)
-                                <a href="{{ $file->getUrl() }}" data-fancybox="images" data-caption="" class="main_item_img">
-                                    <img class="lazy" src="{{ $file->getUrl() }}" style="width: 100%;" alt="">
+                                <a href="{{ $file->getFileUrl() }}" data-fancybox="images" data-caption="" class="main_item_img">
+                                    <img class="lazy" src="{{ $file->getFileUrl() }}" style="width: 100%;" alt="">
                                 </a>
                                 <p class="main_item_p">{{ $file->ru_title }}</p>
                             @elseif(strpos($file->getFileType(), 'video') !== false)
                                 <video preload="metadata" controls style="width: 100%;" alt="" style="width: 100%;">
-                                    <source src="{{ $file->getUrl() }}" type="video/mp4">
+                                    <source src="{{ $file->getFileUrl() }}" type="video/mp4">
                                 </video>
                             @elseif(strpos($file->getFileType(), 'application') !== false)
-                                <a href="{{ $file->getUrl() }}" target="_blank">
+                                <a href="{{ $file->getFileUrl() }}" target="_blank">
                                     <div class="main_item_icon">
                                         <img class="lazy" src="{{ asset('assets/img/pdf-icon.png') }}" alt="">
                                     </div>
