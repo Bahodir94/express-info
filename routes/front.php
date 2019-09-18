869d1f4f281c6826e1ddac9acbb9c43bf7df32d1
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-18 22:33:39 +0500

    Try to use app layout in cgu #3

--- routes/front.php
+++ routes/front.php
@@ -6,11 +6,14 @@
  * Time: 16:42
  */
 
-Route::middleware('needsList')->name('site.')->namespace('Site')->group(function() {
+Route::middleware('needsList')->namespace('Site')->group(function() {
 	Route::get('/cgu-info', 'CguController@cguInfo')->name('home.cgu.info');
 	Route::get('/cgu-info/{id}', 'CguController@cguCategory')->name('home.cgu.info.category');
 	Route::get('/cgu-ad', 'CguController@cguAd')->name('home.cgu.ad');
 	Route::get('/cgu-ad/{id}', 'CguController@cguCategory')->name('home.cgu.ad.category');
+});
+
+Route::middleware('needsList')->name('site.')->namespace('Site')->group(function() {
     // Catalog routes
     Route::get('/', 'CatalogController@index')->name('catalog.index');
     Route::get('/{params}', 'CatalogController@catalog')->where('params', '.+')->name('catalog.main');
