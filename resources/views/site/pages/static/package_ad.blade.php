0aa28f7544fc5cc4cd04a7a0926e1ce22934d020
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-20 20:19:25 +0500

    Add static pages

---
+++ resources/views/site/pages/static/package_ad.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<!-- saved from url=(0052)http://content.mi.vid.uz/land/43-reklama-na-paketakh -->
+<html lang="ru-ru" dir="ltr" vocab="http://schema.org/"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="http://content.mi.vid.uz/templates/yootheme/vendor/yootheme/theme-joomla/assets/images/favicon.png">
+        <link rel="apple-touch-icon-precomposed" href="http://content.mi.vid.uz/templates/yootheme/vendor/yootheme/theme-joomla/assets/images/apple-touch-icon.png">
+        
+	<!--<base href="http://content.mi.vid.uz/land/43-reklama-na-paketakh">--><base href=".">
+	<meta name="author" content="Super User">
+	<meta name="generator" content="Joomla! - Open Source Content Management">
+	<title>Реклама на пакетах</title>
+	<link href="./Реклама на пакетах_files/styles-a7b9287b.css" rel="stylesheet" id="styles-css">
+	<script src="./Реклама на пакетах_files/scripts-6dd67b93.js"></script>
+	<script>
+document.addEventListener('DOMContentLoaded', function() {
+    Array.prototype.slice.call(document.querySelectorAll('a span[id^="cloak"]')).forEach(function(span) {
+        span.innerText = span.textContent;
+    });
+});
+	</script>
+
+    </head>
+    <body class="" cz-shortcut-listen="true">
+
+        
+        
+        <div class="tm-page">
+
+                        
+
+
+
+
+            
+            
+
+            
+            <div id="system-message-container" data-messages="[]">
+</div>
+
+            
+<div class="uk-section-default uk-light">
+        <div style="background-image: url(&quot;/templates/yootheme/cache/3175-f2d3e635.webp&quot;); min-height: calc((100vh - 0px) - 20vh);" class="uk-background-norepeat uk-background-cover uk-background-center-center uk-section uk-flex uk-flex-middle" uk-height-viewport="offset-top: true; offset-bottom: 20;">    
+        
+        
+        
+                        <div class="uk-width-1-1">
+            
+            <div class="uk-container"><div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
+<div class="uk-width-1-1@m uk-first-column">
+    
+        
+            
+                
+                
+                    
+<h1 class="uk-heading-large uk-text-left">        <b>Реклама на пакетах    </b></h1><div class="uk-text-primary uk-margin"><b>Это, пожалуй, самый дешевый и доступный способ заявить о себе.</b></div><b>
+                
+            
+        
+    
+</b></div><b>
+</b></div></div><b>
+                        </b></div><b>
+            
+        
+        </b></div><b>
+    
+</b></div><b>
+
+<div class="uk-section-default uk-section">
+    
+        
+        
+        
+            
+            <div class="uk-container"><div class="uk-grid-margin uk-grid uk-grid-stack" uk-grid="">
+<div class="uk-width-1-1@m uk-first-column">
+    
+        
+            
+                
+                
+                    
+<h1>        Прайс лист    </h1>
+<div class="uk-overflow-auto">    <table class="uk-table">
+                <thead>
+            <tr>
+
+                <th class="uk-text-nowrap">Наименование</th><th>Информация</th><th class="uk-text-nowrap">Цена</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+                <tr class="el-item"><td class="uk-text-nowrap uk-table-shrink"><div class="el-title">1 тираж</div></td><td><div class="el-content uk-panel">реклама на пакетах</div></td><td class="uk-text-nowrap uk-table-shrink"><div class="el-meta uk-text-meta">80 000 сум</div></td></tr>
+                <tr class="el-item"><td class="uk-text-nowrap"><div class="el-title">1 модуль</div></td><td><div class="el-content uk-panel">размер пакета 30 x 40 см</div></td><td class="uk-text-nowrap"><div class="el-meta uk-text-meta">3 400 000 сум</div></td></tr>
+                </tbody>
+
+    </table>
+
+</div>
+
+                
+            
+        
+    
+</div>
+</div></div>
+            
+        
+    
+</div>
+
+<div class="uk-section-default uk-section">
+    
+        
+        
+        
+            
+            <div class="uk-container"></div>
+            
+        
+    
+</div>
+
+
+            
+            
+
+            
+
+
+        </b></div><b>
+
+        
+        
+
+    
+
+</b></body></html>
