0bb119bbca1586caead6125f190da3e689fa09ba
devseylor <48056243+devseylor@users.noreply.github.com>
2019-09-24 17:52:15 +0500

    Разкоментил кнопки на publicservis

--- resources/views/site/pages/main.blade.php
+++ resources/views/site/pages/main.blade.php
@@ -198,12 +198,12 @@
 			<a href="#" class="mobile_main_links_inner_btn mobile_main_links_inner_btn_contacts">
 				Наши Контакты
 			</a>
-			<!--<a href="#" class="mobile_main_links_inner_btn">
+			<a href="#" class="mobile_main_links_inner_btn">
 				Информация ЦГУ
 			</a>
 			<a href="#" class="mobile_main_links_inner_btn">
 				Реклама в ЦГУ
-			</a>-->
+			</a>
 		</div>
 	</div>
 </div>
