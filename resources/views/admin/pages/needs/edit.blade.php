f0995cc4e6c00cc4e59b375cc38d004c99902553
dmitriyl99 <dmitriyl1899@gmail.com>
2019-09-28 00:53:36 +0500

    Add slug fields for types of needs

--- resources/views/admin/pages/needs/edit.blade.php
+++ resources/views/admin/pages/needs/edit.blade.php
@@ -51,6 +51,16 @@
                                 </div>
                                 @error('ru_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
                             </div>
+                            <div class="form-group @error('ru_slug') is-invalid @enderror">
+                                <div class="form-material floating">
+                                    <input class="form-control" type="text" id="ru_slug" name="ru_slug" value="{{ $need->ru_slug }}">
+                                    <label for="ru_slug" @error('ru_slug') class="col-form-label" @enderror>
+                                    Slug
+                                    @error('ru_slug') <span class="text-danger">*</span> @enderror
+                                    </label>
+                                </div>
+                                @error('ru_slug') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
+                            </div>
                         </div>
                         <!-- END Step 1 -->
 
@@ -66,6 +76,16 @@
                                 </div>
                                 @error('en_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
                             </div>
+                            <div class="form-group @error('en_slug') is-invalid @enderror">
+                                <div class="form-material floating">
+                                    <input class="form-control" type="text" id="en_slug" name="en_slug" value="{{ $need->en_slug }}">
+                                    <label for="en_slug" @error('en_slug') class="col-form-label" @enderror>
+                                    Slug
+                                    @error('en_slug') <span class="text-danger">*</span> @enderror
+                                    </label>
+                                </div>
+                                @error('en_slug') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
+                            </div>
                         </div>
                         <!-- END Step 2 -->
 
@@ -80,6 +100,16 @@
                                     </label>
                                 </div>
                                 @error('uz_title') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
+                            </div>
+                            <div class="form-group @error('uz_slug') is-invalid @enderror">
+                                <div class="form-material floating">
+                                    <input class="form-control" type="text" id="uz_slug" name="uz_slug" value="{{ $need->uz_slug }}">
+                                    <label for="uz_slug" @error('uz_slug') class="col-form-label" @enderror>
+                                    Slug
+                                    @error('uz_slug') <span class="text-danger">*</span> @enderror
+                                    </label>
+                                </div>
+                                @error('uz_slug') <div id="val-username-error" class="invalid-feedback animated fadeInDown">{{ $message }}</div> @enderror
                             </div>
                         </div>
                         <!-- END Step 3 -->
