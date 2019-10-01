c16c0abe84311deb07aa5f830c966aaa4069bc48
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-01 15:00:59 +0500

    Render history title from type

--- app/Models/HistoryItem.php
+++ app/Models/HistoryItem.php
@@ -19,4 +19,46 @@ class HistoryItem extends Model
     {
         return $this->hasOne(User::class, 'id', 'user_id');
     }
+
+    /**
+     * Get title for history item
+     *
+     * @return string
+    */
+    public function getTitle()
+    {
+        $type = $this->type;
+        parse_str($this->meta, $metaArray);
+        switch ($type) {
+            case 'company.create': {
+                $url = route('admin.companies.edit', $metaArray['id']);
+                $name = $metaArray['ru_title'];
+                return "Создал(a) компанию <a href='$url' class='link-effect'>$name</a>";
+            }
+            case 'company.update': {
+                $url = route('admin.companies.edit', $metaArray['id']);
+                $name = $metaArray['ru_title'];
+                return "Изменил(a) компанию <a href='$url' class='link-effect'>$name</a>";
+            }
+            case 'company.delete': {
+                $name = $metaArray['ru_title'];
+                return "Удали(а) компанию $name";
+            }
+            case 'category.create': {
+                $url = route('admin.handbookcategories.edit', $metaArray['id']);
+                $name = $metaArray['ru_title'];
+                return "Создал(a) категорию справочника <a href='$url' class='link-effect'>$name</a>";
+            }
+            case 'category.update': {
+                $url = route('admin.handbookcategories.edit', $metaArray['id']);
+                $name = $metaArray['ru_title'];
+                return "Изменил(a) категорию справочника <a href='$url' class='link-effect'>$name</a>";
+            }
+            case 'category.delete': {
+                $name = $metaArray['ru_title'];
+                return "Удалил(а) категорию $name";
+            }
+            default: return 'Undefined';
+        }
+    }
 }
