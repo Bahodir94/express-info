ef79b7a9405e656cbba2e1a78c88bdd17179d32c
dmitriyl99 <dmitriyl1899@gmail.com>
2019-10-19 02:05:50 +0500

    Fix grid

--- resources/views/admin/pages/banners/index.blade.php
+++ resources/views/admin/pages/banners/index.blade.php
@@ -9,7 +9,7 @@
     <h3 class="content-heading">Рекламные баннеры</h3>
     <div class="row js-appear-enabled animated fadeIn" data-toggle="appear">
         @foreach($banners as $banner)
-            <div class="col-4">
+            <div class="col-md-4 col-sm-6">
                 <a href="javascript:void(0)" class="block block-link-shadow text-center">
                     <div class="block-content block-content-full">
                         <div class="font-size-sm font-w600 text-uppercase text-muted">{{ $banner->name }}</div>
