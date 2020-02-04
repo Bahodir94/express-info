a54075f89c8ca3cc1a4044f4dd3c28dbd49f35ee
Surovov <maructp.6ata@gmail.com>
2020-02-05 00:26:05 +0500

    Внедрение шаблона в основной Blade
    Почистил старые основные JS и CSS. Оставил ссылки на шрифты, и пока что оставил библиотеки избранных компаний.

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -6,11 +6,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     @yield('meta')
-    <link rel="stylesheet" href="{{ asset('assets/css/uikit.min.css') }}">
+    
+    
+    <link rel="shortcut icon" type="image/png" href="https://via.placeholder.com/16x16" >
+
+    <link rel="stylesheet" href="{{ asset('assets/css/css/main.css') }}" />
+
     <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700,900&display=swap&subset=cyrillic" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    
     <link rel="stylesheet" href="{{ asset('assets/css/honeycombs.css') }}">
     @yield('css')
     <!-- Icons -->
@@ -22,41 +27,17 @@
     <title>
         @yield('title') | TezInfo
     </title>
-
-
-
-
-    <script src="{{ asset('assets/js/uikit.min.js') }}"></script>
-    <script src="{{ asset('assets/js/uikit-icons.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.js') }}"></script>
-    <script src="https://code.iconify.design/1/1.0.3/iconify.min.js"></script>
-    <script src="{{ asset('assets/js/script.js') }}"></script>
+    <script src="{{ asset('assets/js/uikit.js') }}"></script>
     <script src="{{ asset('assets/js/honeycombs.js') }}"></script>
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-       m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-       ym(54773344, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-       });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/54773344" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
+    @yield('header')
 
+    @include('site.layouts.partials.mobile_menu')
 
-@yield('header')
+    @yield('content')
 
-@include('site.layouts.partials.mobile_menu')
-
-@yield('content')
-
-@include('site.layouts.partials.footer')
+    @include('site.layouts.partials.footer')
 </body>
 </html>
