6066af34c8e0854abeb1dfcef7091bc3c8457c6b
Alisher <galert184@gmail.com>
2020-03-04 22:28:45 +0500

    First commit

---
+++ app/Repositories/BlogCategoryRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory;
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class BlogCategoryRepository.
+ */
+class BlogCategoryRepository implements BlogCategoryRepositoryInterface
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    /**
+     * @param $blogcategory_id
+     * @return mixed
+     */
+    public function get($blogcategory_id)
+    {
+        return BlogCategory::find($blogcategory_id);
+    }
+
+    /**
+     * Get's all posts.
+     *
+     * @return mixed
+     */
+    public function all()
+    {
+        return BlogCategory::all();
+    }
+
+
+    /**
+     * @param $blogcategory_id
+     */
+    public function delete($blogcategory_id)
+    {
+        $category = $this->get($blogcategory_id);
+
+       /* $blogcat_id = null;
+        if($category->blogcat_id != null)
+            $blogcat_id = $category->blogcat_id;*/
+
+        $category->remove();
+
+       // return $blogcat_id;
+    }
+
+
+    /**
+     * @param $blogcategory_id
+     * @param object $blogcategory_data
+     */
+    public function update($blogcategory_id, object $blogcategory_data)
+    {
+        $category = BlogCategory::find($blogcategory_id);
+        $category->update($blogcategory_data->all());
+
+        return $category;
+    }
+
+    /**
+     * @param object $blogcategory_data
+     * @return mixed
+     */
+    public function store(object $blogcategory_data)
+    {
+        $category = BlogCategory::create($blogcategory_data->all());
+
+        return $category;
+    }
+}
---
+++ app/Repositories/BlogCategoryRepositoryInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories;
+
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+//use Your Model
+
+/**
+ * Class BlogCategoryRepositoryInterface.
+ */
+interface BlogCategoryRepositoryInterface
+{
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function get($blogcategory_id);
+
+    public function all();
+
+    public function delete($blogcategory_id);
+
+    public function update($blogcategory_id, object $blogcategory_data);
+
+    public function store(object $blogcategory_data);
+
+}
