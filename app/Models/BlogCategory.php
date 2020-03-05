ae6a27eeef88d5be3d81e23e5a5fe27f1e894030
dmitriyl99 <dmitriyl1899@gmail.com>
2020-03-05 14:47:23 +0500

    Delete unused createSlug methods

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -2,49 +2,17 @@
 
 namespace App;
 
-use App\Models;
-use Cviebrock\EloquentSluggable\Services\SlugService;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
-use Kalnoy\Nestedset\NodeTrait;
 
 class BlogCategory extends Model
 {
-    use NodeTrait;
+    use Sluggable;
 
     protected $fillable = [
         'ru_title', 'en_title', 'uz_title',
         'ru_slug', 'en_slug', 'uz_slug'
     ];
-
-    /**
-     * Возвращает дочерние объекты категории
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function categories()
-    {
-        return $this->hasMany(self::class);
-    }
-
-    /**
-     * Проверяет есть ли дочерние категории или нет
-     *
-     * @return bool
-     */
-    public function hasCategories()
-    {
-        return (isset($this->categories[0])) ? true : false;
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasParentCategory()
-    {
-        return ($this->parentCategory != null) ? true : false;
-    }
 
     /**
      * Возвращаем очищенный заголовок
@@ -54,63 +22,6 @@ class BlogCategory extends Model
     public function getTitle()
     {
         return strip_tags($this->ru_title);
-    }
-
-    /**
-     * Создает слаг для русского языка если не ввели слаг
-     *
-     * @param $title
-     */
-    public function createRuSlug($title)
-    {
-        $this->ru_slug = SlugService::createSlug(BlogCategory::class, 'ru_slug', $title);
-    }
-
-    /**
-     * Создает слаг для английского языка если не ввели слаг
-     *
-     * @param $title
-     */
-    public function createEnSlug($title)
-    {
-        $this->en_slug = SlugService::createSlug(BlogCategory::class, 'en_slug', $title);
-    }
-
-    /**
-     * Создает слаг для узбекского языка если не ввели слаг
-     *
-     * @param $title
-     */
-    public function createUzSlug($title)
-    {
-        $this->uz_slug = SlugService::createSlug(BlogCategory::class, 'uz_slug', $title);
-    }
-
-    /**
-     * @param $title
-     */
-    public function saveRuSlug($title)
-    {
-        $this->ru_slug = $title;
-        $this->save();
-    }
-
-    /**
-     * @param $title
-     */
-    public function saveEnSlug($title)
-    {
-        $this->en_slug = $title;
-        $this->save();
-    }
-
-    /**
-     * @param $title
-     */
-    public function saveUzSlug($title)
-    {
-        $this->uz_slug= $title;
-        $this->save();
     }
 
     /**
