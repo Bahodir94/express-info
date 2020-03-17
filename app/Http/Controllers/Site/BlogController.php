f1c8a40b128792113a8c4d0c1002654d32ab8b40
dmitriyl99 <dmitriyl1899@gmail.com>
2020-03-17 16:54:41 +0500

    Process blog in controller

--- app/Http/Controllers/Site/BlogController.php
+++ app/Http/Controllers/Site/BlogController.php
@@ -39,4 +39,38 @@ class BlogController extends Controller
         return view('blog.index', compact('blogcategories', 'blogposts'));
     }
 
+    /**
+     * Display a category or post of the blog
+     *
+     * @param Request $request
+     * @param string $params
+     * @return void
+     */
+    public function blog(Request $request, string $params)
+    {
+        $paramsArray = explode('/', trim($params, '/'));
+        $slug = end($paramsArray);
+
+        $blogCategory = $this->blogCategories->getBySlug($slug);
+        if ($blogCategory)
+            return $this->processBlogCategory($request, $blogCategory);
+        $blogPost = $this->blogPosts->getBySlug($slug);
+        if ($blogPost)
+            return $this->processBlogPost($request, $blogPost);
+        else
+            abort(404);
+    }
+
+    private function processBlogCategory(Request $request, \App\Models\BlogCategory $category)
+    {
+        $categories = $this->blogCategories->all();
+        return view('site.pages.blog.category', compact('category', 'categories'));
+    }
+
+    private function processBlogPost(Request $request, \App\Models\BlogPost $post)
+    {
+        return view('site.pages.blog.post', compact('post'));
+    }
+
+
 }
