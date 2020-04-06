2e11d1e724d05cb36bb8a7e3de81f3f89a261ff6
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-06 18:10:06 +0500

    Add buttons for auth users

--- resources/views/site/layouts/partials/headers/default.blade.php
+++ resources/views/site/layouts/partials/headers/default.blade.php
@@ -7,18 +7,22 @@
                     </button>
                 </div>
                 <div class="header-logo"><a class="qdesk-logo" href="#" title="VID"><img class="qdesk-logo-white"
-                                                                                           src="{{ asset('front/images/VID.png') }}"
-                                                                                           alt="VID"><img
-                            class="qdesk-logo-black" src="{{ asset('front/images/VID-black.png') }}}" alt="VID"></a></div>
+                                                                                         src="{{ asset('front/images/VID.png') }}"
+                                                                                         alt="VID"><img
+                            class="qdesk-logo-black" src="{{ asset('front/images/VID-black.png') }}}" alt="VID"></a>
+                </div>
                 <div class="navigation" id="navigation">
                     <ul class="main-menu">
                         <li class="active"><a href="/">Главная</a></li>
                         @foreach($needs as $need)
-                            <li class="header-menu-item"><a href="#">{{ $need->ru_title }} <i class="fas fa-caret-down"></i></a>
+                            <li class="header-menu-item"><a href="#">{{ $need->ru_title }} <i
+                                        class="fas fa-caret-down"></i></a>
                                 <ul class="sub-menu">
                                     @foreach($need->menuItems as $item)
                                         <li class="menu-item dropdown-submenu">
-                                            <a class="d-flex justify-content-between align-items-center" href="{{ route('site.catalog.main', $item->ru_slug) }}">{{ $item->ru_title }}<i class="fas fa-caret-right ml-2 mr-3"></i></a>
+                                            <a class="d-flex justify-content-between align-items-center"
+                                               href="{{ route('site.catalog.main', $item->ru_slug) }}">{{ $item->ru_title }}
+                                                <i class="fas fa-caret-right ml-2 mr-3"></i></a>
                                             <ul class="sub-menu">
                                                 @foreach($item->categories as $category)
                                                     <li>
@@ -36,11 +40,34 @@
                 </div>
             </div>
             <div class="header-right">
-                <ul>
-                    <li><a href="{{ route('site.tenders.common.create') }}"><i class="fas fa-plus-circle"></i> Добавить заказ</a></li>
-                    <li><a href="{{ route('login')}}"><i class="fas fa-sign-out-alt"></i> Вход</a><span> / </span><a
-                            href="{{ route('register')}}">Регистрация</a></li>
-                </ul>
+                @guest
+                    <ul>
+                        <li><a href="{{ route('site.tenders.common.create') }}"><i class="fas fa-plus-circle"></i>
+                                Добавить заказ</a></li>
+                        <li><a href="{{ route('login')}}"><i class="fas fa-sign-out-alt"></i> Вход</a><span> / </span><a
+                                href="{{ route('register')}}">Регистрация</a></li>
+                    </ul>
+                @endguest
+                @auth
+                    <ul>
+                        @if (auth()->user()->hasRole('customer')) <li><a href="{{ route('site.tenders.common.create') }}"><i class="fas fa-plus-circle"></i>
+                                Добавить заказ</a></li>@endif
+                        <li>
+                            <a href="#" id="navBarDropdown" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item text-primary" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Выйти
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    </ul>
+                @endauth
             </div>
         </div>
     </div>
