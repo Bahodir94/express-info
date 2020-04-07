d98d1841732282bf412bd04c88bff2a467bc2036
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-07 22:58:55 +0500

    Fix links

--- resources/views/site/layouts/partials/footer.blade.php
+++ resources/views/site/layouts/partials/footer.blade.php
@@ -4,7 +4,6 @@
             <div class="row align-items-center text-center text-lg-left">
                 <div class="col-lg-4 mb-2 mb-lg-0"><img src="/front/images/VID.png" alt="VID"></div>
                 <div class="col-lg-8">
-
                     <ul class="nav-footer">
                         <li class="pl-4"><a href="#">Политика конфиценциальности</a></li>
                         <li><a href="#">Пользовательское соглашение</a></li>
@@ -23,24 +22,22 @@
                                     <li><a href="{{ route('site.account.index') }}">Мой кабинет</a></li>
                                 @endauth
                                 @guest
-                                    <li><a href="#">Войти</a></li>
-                                    <li><a href="#">Создать аккаунт</a></li>
+                                    <li><a href="{{ route('login') }}">Войти</a></li>
+                                    <li><a href="{{ route('register') }}">Создать аккаунт</a></li>
                                 @endguest
                             </ul>
                         </div>
                         <div class="col-6 col-md-4">
                             <h3 class="title-footer">Конкурсы</h3>
                             <ul class="links-footer">
-                                <li><a href="#">Создание конкурса</a></li>
-                                <li><a href="#">Каталог конкурсов</a></li>
+                                <li><a href="{{ route('site.tenders.common.create') }}">Создание конкурса</a></li>
+                                <li><a href="{{ route('site.tender.index') }}">Каталог конкурсов</a></li>
                             </ul>
                         </div>
                         <div class="col-6 col-md-4">
                             <h3 class="title-footer">Компания</h3>
                             <ul class="links-footer">
-                                <li><a href="#">О нас</a></li>
                                 <li><a href="{{ route('site.blog.index') }}">Блог</a></li>
-                                <li><a href="#">Контакты</a></li>
                             </ul>
                         </div>
                     </div>
