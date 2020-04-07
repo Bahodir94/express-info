838ef4748973d475b6009a3c320834dcda61a98b
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-07 20:46:13 +0500

    Add tender list page

---
+++ resources/views/site/pages/account/customer/tenders.blade.php
@@ -0,0 +1,80 @@
+@extends('site.layouts.account')
+
+@section('title', 'Тендеры')
+
+@section('header')
+    @include('site.layouts.partials.headers.default')
+@endsection
+
+@section('account.title.h1', 'Управление тендерами')
+
+@section('account.content')
+    <section class="box-admin">
+        <div class="header-box-admin">
+            <h3>Мои конкурсы</h3>
+        </div>
+        <div class="body-box-admin p-0">
+            <div class="table-responsive">
+                <table class="table tbl-job">
+                    <thead>
+                        <tr>
+                            <th>Название проекта</th>
+                            <th class="d-none d-xl-table-cell text-center">Предложений</th>
+                            <th class="d-none d-xl-table-cell text-center">Категория</th>
+                            <th class="d-none d-xl-table-cell text-center">Статус</th>
+                            <th class="d-none d-md-table-cell text-right">Действия</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($user->ownedTenders as $tender)
+                        <tr class="my-job-item">
+                            <td>
+                                <h3 class="title-job"><a href="#">{{ $tender->title }}</a></h3>
+                                <div class="meta-job"><span> <i class="fas fa-calendar-alt"></i>Опубликован {{ $tender->created_at }}</span><span> <i
+                                            class="fas fa-calendar-alt"></i>Истекает {{ $tender->deadline }}</span></div>
+                                <div class="salary-job"><i class="fas fa-money-bill-alt"></i>{{ $tender->budget }} сум
+                                </div>
+                                <div class="job-info d-xl-none"><span
+                                        class="number-application">{{ $tender->requests()->count() }} заявок</span><span
+                                        class="active">@if ($tender->opened) Активный @endif</span></div>
+                                <div class="job-func d-flex d-md-none">
+                                    <a class="btn btn-light btn-edit"><i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                    <form action="{{ route('site.tenders.delete', $tender->id) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="hidden" name="redirect_to" value="{{ route('site.account.tenders') }}">
+                                        <button type="submit" onclick="return confirm('Вы уверены, что хотите безвозвратно удалить конкурс {{ $tender->title }}?')" class="btn btn-light btn-delete"><i class="far fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                            <td class="d-none d-xl-table-cell text-center number-application">{{ $tender->requests()->count() }} заявок
+                            </td>
+                            <td class="d-none d-xl-table-cell text-center">
+                                @foreach($tender->categories as $category)
+                                    <div>{{ $category->getTitle() }} </div>
+                                @endforeach
+                            </td>
+                            <td class="d-none d-xl-table-cell text-center active">@if ($tender->opened) Активный @endif</td>
+                            <td class="d-none d-md-table-cell text-right">
+                                <div class="d-flex">
+                                    <a href="{{ route('site.account.tenders.edit', $tender->slug) }}" class="btn btn-light btn-edit"><i class="fas fa-pencil-alt"></i>
+                                    </a>
+                                    <form action="{{ route('site.tenders.delete', $tender->id) }}">
+                                        @csrf
+                                        @method('delete')
+                                        <input type="hidden" name="redirect_to" value="{{ route('site.account.tenders') }}">
+                                        <button type="submit" onclick="return confirm('Вы уверены, что хотите безвозвратно удалить конкурс {{ $tender->title }}?')" class="btn btn-light btn-delete"><i class="far fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
