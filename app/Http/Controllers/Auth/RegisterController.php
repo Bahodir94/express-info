3ecfd4e8fe216208bbc555f2e00bfea977cdfeb4
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-08 12:13:22 +0500

    Registration form only with email and password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -76,10 +76,6 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        abort_if($data['user_role'] === 'admin', 403);
-        $role = Role::where('name', $data['user_role'])->first();
-        abort_if(!$role, 400);
-        $user->roles()->attach($role->id);
         $user->markEmailAsVerified();
         return $user;
     }
