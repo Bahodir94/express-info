e19ad34724e5cd44e85aeb65e8aaab3779585cc3
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-08 20:18:26 +0500

    Fix index name

--- database/migrations/2020_04_08_200921_drop_unique_user_email.php
+++ database/migrations/2020_04_08_200921_drop_unique_user_email.php
@@ -14,7 +14,7 @@ class DropUniqueUserEmail extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropUnique('email');
+            $table->dropUnique('users_email_unique');
         });
     }
 
@@ -26,7 +26,7 @@ class DropUniqueUserEmail extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unique('email');
+            $table->unique('users_email_unique');
         });
     }
 }
