77270f2cfbbc2108fe12775aaf15a146685692fb
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-08 19:30:14 +0500

    Add telegram login button

--- resources/views/registration/register.blade.php
+++ resources/views/registration/register.blade.php
@@ -58,7 +58,7 @@
                 <div class="sign-up-other">
                     <div class="text-or">Или</div>
                     <div class="sign-in-social row row-md">
-                        <div class="col-md-6"><script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="vid_registration_bot" data-size="medium" data-auth-url="/account/create" data-request-access="write"></script></div>
+                        <div class="col-md-6"><script async src="https://telegram.org/js/telegram-widget.js?7" data-telegram-login="vid_registration_bot" data-size="large" data-auth-url="/auth/telegram/callback"></script></div>
 
                     </div>
                 </div>
