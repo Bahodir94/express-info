ab80ae8e2a82b8f3548e6dc22501a4b1d341ba6c
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-08 19:58:28 +0500

    Add logout form

--- resources/views/site/layouts/partials/headers/default.blade.php
+++ resources/views/site/layouts/partials/headers/default.blade.php
@@ -129,6 +129,9 @@
                                                      document.getElementById('logout-form').submit();">
                           <i class="fas fa-sign-out-alt"></i> Выйти
                       </a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          @csrf
+                      </form>
                   </div>
               </li>
           @endguest
