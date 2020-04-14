b5d326042eba0a83e57e499786706781d673b8f8
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-14 22:00:31 +0500

    Add meta prefixes for menu items

--- database/migrations/2020_04_14_215933_menu_items_tenders_meta_fields.php
+++ database/migrations/2020_04_14_215933_menu_items_tenders_meta_fields.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CategoriesTendersMetaFields extends Migration
+class MenuItemsTendersMetaFields extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CategoriesTendersMetaFields extends Migration
      */
     public function up()
     {
-        Schema::table('handbook_categories', function (Blueprint $table) {
+        Schema::table('menu_items', function (Blueprint $table) {
             $table->string('tender_meta_title_prefix')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class CategoriesTendersMetaFields extends Migration
      */
     public function down()
     {
-        Schema::table('handbook_categories', function (Blueprint $table) {
+        Schema::table('menu_items', function (Blueprint $table) {
             $table->dropColumn('tender_meta_title_prefix');
         });
     }
