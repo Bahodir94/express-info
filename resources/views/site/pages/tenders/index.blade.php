9073bd34fb52a3374fd1a875aa44b57b57473e65
dmitriyl99 <dmitriyl1899@gmail.com>
2020-04-17 19:10:55 +0500

    Change text after deadline

--- resources/views/site/pages/tenders/index.blade.php
+++ resources/views/site/pages/tenders/index.blade.php
@@ -107,7 +107,7 @@
                                         </div>
                                         <div class="col-md-10 job-info">
                                             <div class="text">
-                                                <h3 class="title-job"><a href="{{ route('site.tenders.category', $tender->slug) }}">{{ $tender->title }}</a><span class="ml-2 tags"><a>@if (!$tender->checkDeadline() || $tender->contractor) Срок приёма заявок окончен @else Открыт @endif</a></span></h3>
+                                                <h3 class="title-job"><a href="{{ route('site.tenders.category', $tender->slug) }}">{{ $tender->title }}</a><span class="ml-2 tags"><a>@if (!$tender->checkDeadline() || $tender->contractor) Приём заявок окончен @else Открыт @endif</a></span></h3>
                                                 <div class="date-job">
                                                   <i class="fa fa-check-circle"></i><span
                                                         class="company-name">Опубликован: {{ $tender->created_at->format('d.m.Y') }}</span>
