5d87ee80db509653f94eb9a64da458b05c6b3c41
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-08 19:42:22 +0500

    Customize email verification texts

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,7 +2,7 @@
     {{-- Header --}}
     @slot('header')
         @component('mail::header', ['url' => config('app.url')])
-            {{ config('app.name') }}
+            VID.UZ
         @endcomponent
     @endslot
 
@@ -21,7 +21,7 @@
     {{-- Footer --}}
     @slot('footer')
         @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. Все права защищены
+            © {{ date('Y') }} VID.UZ. Все права защищены
         @endcomponent
     @endslot
 @endcomponent
