97fc377c93f921a13559fd47c90e355f58a8d8e9
dmitriyl99 <dmitriyl1899@gmail.com>
2020-05-11 22:26:18 +0500

    Add cron task for check tenders deadlines

---
+++ app/Console/Commands/CheckTendersDeadline.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Tender;
+use Illuminate\Console\Command;
+
+class CheckTendersDeadline extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tenders:checkdeadline';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command check all tenders\' deadlines and delete requests';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $tenders = Tender::where('deadline', '<', now())->whereNull('contractor_id')->get();
+        foreach ($tenders as $tender) {
+            $tender->requests()->delete();
+        }
+    }
+}
