9e74e087b48904fb237d7ab63fb5d4ef9be75af5
Vvladimir97 <vladmir97vova@gmail.com>
2020-05-13 21:18:12 +0500

    change sort tenderrepository.php

--- app/Repositories/TenderRepository.php
+++ app/Repositories/TenderRepository.php
@@ -27,7 +27,8 @@ class TenderRepository implements TenderRepositoryInterface
         $query = Tender::whereNotNull('owner_id')->where('published', true);
         if ($withoutContractors)
             $query = $query->whereNull('contractor_id');
-        return $query->orderBy('created_at', 'desc')->get();
+        return $query->orderByRaw('-contractor_id asc')->orderBy('deadline', 'desc')->orderBy('created_at', 'desc')->get();
+        //orderBy('created_at', 'desc')
     }
 
     /**
